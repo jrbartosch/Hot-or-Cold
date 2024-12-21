c21d2783bfd76b0df7d9a25329e7cc64801dcb67
jrbartosch <jbartosch+jed@gmail.com>
2024-12-21 16:42:05 +0600

    Add files via upload

---
+++ hot-or-cold.py
@@ -0,0 +1,7 @@
+temp = float(input ('Please Enter the Temperature in Farenheit.'))
+if temp <= 32:
+    print ('Wear warm clothing.')
+elif temp >= 70:
+    print ('Wear light clothing.')
+else:
+    print ('Wear normal clothing.')
